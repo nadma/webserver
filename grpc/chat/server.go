3c1ef5af02a0285fa0482ce9a1f9a57cadd00f7a
martin <martin.nad@jawep.com>
2021-02-12 04:47:26 +0100

    adding grpc-hello

---
+++ grpc/chat/server.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/tutorialedge/go-grpc-beginners-tutorial/chat"
+	"google.golang.org/grpc"
+)
+
+func main() {
+
+	fmt.Println("Go gRPC Beginners Tutorial!")
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 9000))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := chat.Server{}
+
+	grpcServer := grpc.NewServer()
+
+	chat.RegisterChatServiceServer(grpcServer, &s)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %s", err)
+	}
+}
+	// go mod init github.com/tutorialedge/go-grpc-tutorial
+	//in this directory
+	//and improt github.com/tutorialedge/go-grpc-tutorial/chat
+	//then line 21 add
+	//then line 25 add
+}
---
+++ grpc/helloworld/greeter_server/go.mod
@@ -0,0 +1,8 @@
+module main
+
+go 1.15
+
+require (
+	google.golang.org/grpc v1.35.0
+	google.golang.org/grpc/examples v1.0.1
+)
